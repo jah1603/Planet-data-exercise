87a69b2ec4ec3354ee2940fa6fe22f66397a638d
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-07 13:39:50 +0100

    planet model ready

--- src/data/planets.js
+++ src/data/planets.js
@@ -1,4 +1,6 @@
-const planets = [
+const Planets = function () {
+
+this.planets = [
   {
     name: 'Mercury',
     orbit: 87.969,
@@ -80,5 +82,25 @@ const planets = [
     image: 'images/neptune.jpg'
   }
 ];
+}
 
-module.exports = planets;
+
+Planets.prototype.bindEvents = function(){
+  PubSub.publish('Planetss:planet-list-ready', this.planets);
+
+  PubSub.subscribe('SelectView:click', (evt) => {
+    const selectedIndex = evt.detail;
+    this.publishPlanetDetail(selectedIndex);
+  });
+};
+
+Planets.prototype.publishPlanetDetail = function(planetIndex){
+  const selectedPlanet = this.planets[planetIndex];
+  PubSub.publish('Planets:selected-planet-ready', selectedPlanet)
+};
+
+
+
+
+
+module.exports = Planets;
